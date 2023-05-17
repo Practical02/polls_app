701f6194ad04ae6941c89b855f6dcf93a0cfa04a
Amal Manoj <amalmanoj02@gmail.com>
2023-05-17 09:12:57 +0530

    BAckend setup

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors =require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = new express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+mongoose.connect(
+    process.env.MONGO_URI
+  );
+  const connection = mongoose.connection;
+  
+  connection.once("open", () => {
+    console.log("MongoDB connection established successfully");
+  });
+
+
+
+
+
+
+
+  app.listen("8080", () => {
+    console.log("Started server on 8080");
+  });
+  
